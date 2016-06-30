f5f80170ce9a7fb2cb8e67978a2752e176169d4a
Jonathan Sullivan <jonathan@sullivanway.net>
2016-06-29 20:24:54 -0400

    5B - Rev 4 - Packages Sep, Constant Variables, Cleanup

--- src/accounting/run_payroll.py
+++ src/accounting/run_payroll.py
@@ -1,82 +1,104 @@
 from src.accounting import hourly_employee
 from src.accounting import salaried_employee
+import tkinter
 
 
-def runpayroll():
+class RunPayroll:
 
-    hourly_employee_process = open('../src/files/hourly_employees.csv', 'r')
-    count = 0
-    hourly_employee_list = {}
-    for line in hourly_employee_process:
-        if count !=0:
-            x = line.split(',')
-            x[4] = x[4].strip()
-            x[5] = x[5].strip()
-            if x [4] == '-':
-                x [4] = 0
-            employee = hourly_employee.HourlyEmployee(x[0], x[1], x[2], x[3], x[4], x[5], '123 Main Street', 'Clearwater', 'FL', '33777')
-            hourly_employee_list[x[0]] = employee
+    def __init__(self):
 
-        count += 1
-    hourly_employee_process.close()
+        hourly_employee_process = open('../src/files/hourly_employees.csv', 'r')
+        count = 0
+        hourly_employee_list = {}
+        EmployeeId = 0
+        LastName = 1
+        FirstName = 2
+        HourlyRate = 3
+        UnionDues = 4
+        PaymentMethod = 5
+        for line in hourly_employee_process:
+            if count !=0:
+                record = line.split(',')
+                record[UnionDues] = record[UnionDues].strip()
+                record[PaymentMethod] = record[PaymentMethod].strip()
+                if record [UnionDues] == '-':
+                    record [UnionDues] = 0
+                employee = hourly_employee.HourlyEmployee(record[EmployeeId], record[LastName], record[FirstName], record[HourlyRate], record[UnionDues], record[PaymentMethod], '123 Main Street', 'Clearwater', 'FL', '33777')
+                hourly_employee_list[record[EmployeeId]] = employee
 
-    time_card_process = open('../src/files/timecards.csv', 'r')
-    count = 0
-    for line in time_card_process:
-        if count !=0:
-            card = line.split(',')
-            card[3] = card[3].strip()
-            hourly_employee_list[card[0]].clock_in(card[3], card[1])
-            hourly_employee_list[card[0]].clock_out(card[3], card[2])
+            count += 1
+        hourly_employee_process.close()
 
-        count += 1
-    time_card_process.close()
+        time_card_process = open('../src/files/timecards.csv', 'r')
+        count = 0
+        for line in time_card_process:
+            if count !=0:
+                card = line.split(',')
+                card[HourlyRate] = card[HourlyRate].strip()
+                hourly_employee_list[card[EmployeeId]].clock_in(card[HourlyRate], card[LastName])
+                hourly_employee_list[card[EmployeeId]].clock_out(card[HourlyRate], card[FirstName])
 
-    for x in hourly_employee_list:
-        hourly_employee_list[x].calculate_pay()
+            count += 1
+        time_card_process.close()
 
-    salaried_employee_process = open('../src/files/salaried_employees.csv', 'r')
-    count = 0
-    salaried_employee_list = {}
-    for line in salaried_employee_process:
-        if count !=0:
-            y = line.split(',')
-            y[5] = y[5].strip()
-            y[6] = y[6].strip()
-            if y[5] == '-':
-                y[5] = 0
-            employee = salaried_employee.SalariedEmployee(y[0], y[1], y[2], y[3], y[4], y[5], y[6], '123 Main Street', 'Clearwater', 'FL', '33777')
-            salaried_employee_list[y[0]] = employee
+        for record in hourly_employee_list:
+            hourly_employee_list[record].calculate_pay()
 
-        count += 1
+        salaried_employee_process = open('../src/files/salaried_employees.csv', 'r')
+        count = 0
+        salaried_employee_list = {}
+        EmployeeId = 0
+        LastName = 1
+        FirstName = 2
+        Salary = 3
+        CommissionRate = 4
+        UnionDues = 5
+        PaymentMethod = 6
+        for line in salaried_employee_process:
+            if count !=0:
+                record = line.split(',')
+                record[UnionDues] = record[UnionDues].strip()
+                record[PaymentMethod] = record[PaymentMethod].strip()
+                if record [UnionDues] == '-':
+                    record [UnionDues] = 0
+                employee = salaried_employee.SalariedEmployee(record[EmployeeId], record[LastName], record[FirstName], record[Salary], record[CommissionRate], record[UnionDues], record[PaymentMethod], '12445 Canal Street', 'Tiere Verde', 'FL', '32796')
+                salaried_employee_list[record[EmployeeId]] = employee
 
-    salaried_employee_process.close()
+            count += 1
 
-    receipt_process = open('../src/files/receipts.csv', 'r')
-    count = 0
-    for line in receipt_process:
-        if count != 0:
-            comma_count = 0
-            line2 = ''
-            for z in range(0, len(line)):
-                if line[z] == ',':
-                    comma_count += 1
-                    if comma_count > 5:
-                        continue
+        salaried_employee_process.close()
+
+        receipt_process = open('../src/files/receipts.csv', 'r')
+        count = 0
+        EmployeeId=0
+        LastName=1
+        Item=2
+        Units=3
+        Unit_Cost=4
+        Total=5
+        for line in receipt_process:
+            if count != 0:
+                comma_count = 0
+                line2 = ''
+                for record in range(0, len(line)):
+                    if line[record] == ',':
+                        comma_count += 1
+                        if comma_count > 5:
+                            continue
+                        else:
+                            line2 = line2 + ','
                     else:
-                        line2 = line2 + ','
-                else:
-                    line2 = line2 + line[z]
+                        line2 = line2 + line[record]
 
-            receipt_data = line2.split(',')
-            total = receipt_data[5].strip()
-            total = total.strip('"')
-            total = total.strip()
-            total_float = float(total)
-            salaried_employee_list[receipt_data[0]].make_sale(receipt_data[0], receipt_data[1], receipt_data[2], int(receipt_data[3]), float(receipt_data[4]), total_float)
+                receipt_data = line2.split(',')
+                total = receipt_data[Total].strip()
+                total = total.strip('"')
+                total = total.strip()
+                total_float = float(total)
+                salaried_employee_list[receipt_data[EmployeeId]].make_sale(receipt_data[EmployeeId], receipt_data[LastName], receipt_data[Item], int(receipt_data[Units]), float(receipt_data[Unit_Cost]), total_float)
 
-        count += 1
-    receipt_process.close()
+            count += 1
+        receipt_process.close()
 
-    for y in salaried_employee_list:
-        salaried_employee_list[y].calculate_pay()
+        for y in salaried_employee_list:
+            salaried_employee_list[y].calculate_pay()
---
+++ src/views/payroll_complete_gui.py
@@ -0,0 +1,35 @@
+from tkinter import *
+import tkinter
+import src.accounting.run_payroll
+import src.views.main_gui
+
+
+class RunPayrollGUI:
+
+    def __init__(self):
+
+        def ok_button():
+            self.main_window.destroy()
+            src.views.main_gui.MainGUI()
+
+
+        self.main_window = tkinter.Tk()
+
+        self.bottom_frame = tkinter.Frame(self.main_window)
+
+        canvas = Canvas(width=1250, height=720, bg='black')
+
+        canvas.pack(expand=YES, fill=BOTH)
+
+        gif2 = PhotoImage(file='../src/images/wallpaper.gif')
+        canvas.create_image(0, 0, image=gif2, anchor=NW)
+
+        start_button = Button(canvas, text="Return to Main Menu", command=ok_button, anchor=W)
+        start_button.configure(width=15, activebackground="#33B5E5", relief=FLAT)
+        start_button_window = canvas.create_window(450, 375, anchor=NW, window=start_button)
+
+        quit_button = Button(canvas, text='Quit', command=self.main_window.destroy, anchor=W)
+        quit_button.configure(width=10, activebackground="#33B5E5", relief=FLAT)
+        quit_button_window = canvas.create_window(1080, 600, anchor=NW, window=quit_button)
+
+        tkinter.mainloop()
